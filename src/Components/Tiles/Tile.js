7c859680d05d0f2860d3ec68e695bff1e5f25f92
raartist <rishabhzee98@gmail.com>
2020-11-12 16:23:59 +0530

    tile changed from func to class

--- src/Components/Tiles/Tile.js
+++ src/Components/Tiles/Tile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
@@ -7,9 +7,10 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import "../../App.css";
 import { Link } from "react-router-dom";
-import { useHistory } from "react-router-dom";
+import { Component } from "react";
+import SubjectView from "../../Views/SubjectView";
 
-const useStyles = makeStyles({
+const useStyles = (theme) => ({
   root: {
     maxWidth: 345,
     padding: 8,
@@ -19,35 +20,50 @@ const useStyles = makeStyles({
   },
 });
 
-export default function MediaCard(props) {
-  const classes = useStyles();
-  const subject = props;
-  const history = useHistory();
+export class Tile extends Component {
+  constructor(props) {
+    super(props);
 
-  const HandleClick = () => {
-    history.push("/physics/view");
-  };
-  return (
-    <Card className={classes.root}>
-      <Link to={"/" + subject.link}>
-        <CardActionArea className="zoom" onClick={HandleClick}>
-          <div>
-            <CardMedia
-              className={classes.media}
-              image={props.src}
-              title="Subject Card"
-            />
-          </div>
-          <CardContent>
-            <Typography gutterBottom variant="h6" component="h5">
-              {props.title}
-            </Typography>
-            <Typography variant="caption" color="textSecondary" component="p">
-              {props.desc}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Link>
-    </Card>
-  );
+    this.state = {
+      subject: this.props,
+      subjectView: true,
+    };
+  }
+
+  render() {
+    const { classes } = this.props;
+    const subject = this.state.subject;
+
+    return (
+      <div>
+        <Card className={classes.root}>
+          <Link to={"/" + subject.link}>
+            <CardActionArea className="zoom">
+              <div>
+                <CardMedia
+                  className={classes.media}
+                  image={subject.src}
+                  title="Subject Card"
+                />
+              </div>
+              <CardContent>
+                <Typography gutterBottom variant="h6" component="h5">
+                  {subject.title}
+                </Typography>
+                <Typography
+                  variant="caption"
+                  color="textSecondary"
+                  component="p"
+                >
+                  {subject.desc}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Link>
+        </Card>
+      </div>
+    );
+  }
 }
+
+export default withStyles(useStyles)(Tile);
